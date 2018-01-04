46ba228be1fd298e3056c887535a67a465823561
Stuart Davison <>
2018-01-04 02:27:02 -0800

    added fluent db phase 1

---
+++ Sources/App/Models/Pet.swift
@@ -0,0 +1,45 @@
+//
+//  Pet.swift
+//  TestPackageDescription
+//
+//  Created by Stuart Davison on 1/4/18.
+//
+
+import FluentProvider
+
+final class Pet: Model {
+	var name: String
+	var age: Int
+	let storage = Storage()
+	
+	init(row: Row) throws {
+		name = try row.get("name")
+		age = try row.get("age")
+	}
+	
+	init(name: String, age: Int) {
+		self.name = name
+		self.age = age
+	}
+	
+	func makeRow() throws -> Row {
+		var row = Row()
+		try row.set("name", name)
+		try row.set("age", age)
+		return row
+	}
+}
+
+extension Pet: Preparation {
+	static func prepare(_ database: Database) throws {
+		try database.create(self) { pets in
+			pets.id()
+			pets.string("name")
+			pets.int("age")
+		}
+	}
+	
+	static func revert(_ database: Database) throws {
+		try database.delete(self)
+	}
+}
--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -1,7 +1,36 @@
 import Vapor
 
+
 extension Droplet {
     func setupRoutes() throws {
+		
+		
+		do {
+
+			print( "BEFORE" )
+			for pet in try Pet.all() {
+				print(pet.name, pet.age)
+			}
+			print( "AFTER" )
+
+			let dog = Pet(name: "Poochie", age: 15)
+			try dog.save()
+			print("Doggy ID:", dog.id, dog.age) // the newly saved pet's id
+	
+			guard let foundDog = try Pet.find(1) else {
+				throw( Abort.notFound )
+			}
+			print( foundDog.name )
+			
+			for pet in try Pet.all() {
+				print(pet.name, pet.age)
+			}
+
+		} catch let error {
+			print( "poop", error )
+		}
+		
+		
         get("hello") { req in
             var json = JSON()
             try json.set("hello", "world")
